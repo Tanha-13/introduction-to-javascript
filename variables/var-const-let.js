713339de474adaef7be91e0c53cafe9c0b5618f6
Tanha <nusrattanha897@gmail.com>
2024-01-06 22:23:48 +0600

    basic concepts of variable declaration

---
+++ variables/var-const-let.js
@@ -0,0 +1,26 @@
+const accountId = 144553;
+let accountEmail = 'tanha@gmail.com';
+var accountPassword = '12345';
+accountCity = "dhaka";
+
+// changing const variable
+// accountId = 2; // value can not be changed
+
+// changing let variable
+accountEmail = 'nusrat@gmail.com' // value can be changed
+
+// changing var variable
+accountPassword = '212121'; // value can be changed
+
+
+accountCity = 'chandpur';
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+console.table([accountEmail,accountId,accountPassword,accountCity])
+
+/**
+ * prefer not to use var because of issue in block scope and functional scope
+ * 
+ */
