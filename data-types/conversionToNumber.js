bab6482edf5663aae8a542207f1649ca3986284c
Tanha <nusrattanha897@gmail.com>
2024-01-08 02:22:48 +0600

    conversion any data to Number

---
+++ data-types/conversionToNumber.js
@@ -0,0 +1,30 @@
+let score = '33';
+
+// non number
+let score1 = null;
+
+
+// declare typeof - 1
+// console.log(typeof score);
+// declare typeof - 2
+// console.log(typeof(score1));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+let value1InNumber = Number(score1);
+// console.log(value1InNumber);
+// console.log(typeof value1InNumber);
+
+/**
+ * Notes
+ * any data converts to number
+ * "33" => 33
+ * "33abc" => NaN
+ * null => 0
+ * undefined => NaN
+ * true => 1
+ * false => 0
+ * "abc" => NaN
+ */
+
