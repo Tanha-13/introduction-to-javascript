e357941a3720ee16b21f01fc839995dcca499112
Tanha <nusrattanha897@gmail.com>
2024-01-08 02:29:32 +0600

    conversion data to boolean

---
+++ data-types/conversionToBoolean.js
@@ -0,0 +1,14 @@
+let isLoggedIn = undefined;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+/**
+ * Notes
+ *  1 => true
+ *  0 => false
+ *  "" => false
+ *  "nusrat" => true
+ *  null => false
+ *  undefined => false
+ */
