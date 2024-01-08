88f253222c3428d7a45681b167df7670efed0c24
Tanha <nusrattanha897@gmail.com>
2024-01-09 00:39:36 +0600

    basic operators

---
+++ data-types/operations.js
@@ -0,0 +1,40 @@
+
+let value = 3;
+let negativeValue = -value;
+// console.log(negativeValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(3**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// *********************Complex Operation**************************
+let str1 ='hello';
+let str2 = ' world';
+// console.log(str1 + str2);
+
+
+// console.log('1' + 2);
+// console.log(1 + '2');
+// console.log('1' + 2 + 2);
+// console.log(1 + 2 + '2');
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+ true);
+// console.log(+"");
+
+let num1=4,num2=4,num3 = 4;
+// console.table([num1,num2,num3]);
+
+let gameCounter = 100;
+gameCounter++; // increment game counter
+gameCounter++; // 
+
+// console.log(gameCounter);
+
+
+
+
